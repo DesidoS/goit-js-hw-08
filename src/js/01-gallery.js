50158fa5cee90543d8ad75d2ad018cf67a3c4b1e
Oleksii Kravchenko <djdesidos@gmail.com>
2022-08-03 14:31:51 +0300

    add simple

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery')
+
+const markup = galleryItems.map(({ preview, original, description}) =>
+`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join("");
+
+galleryContainer.insertAdjacentHTML("beforeend", markup);
+
+
+galleryContainer.addEventListener("click", handleClick)
+
+function handleClick(event) {
+    event.preventDefault();
+    
+    var lightbox = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+const form = document.querySelector('.feedback-form')
+const input = document.querySelector('.feedback-form input')
+const message = document.querySelector('.feedback-form textarea')
 
+form.addEventListener('submit', onSubmit);
+input.addEventListener('input', onInput);
+message.addEventListener('input', onMessage);
+savedTextarea();
+savedEmail();
+
+function onSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem('feedback');
+    localStorage.removeItem('email');
+}
+
+function onInput(e) {
+    const inpValue = e.currentTarget.value;
+    localStorage.setItem('email', inpValue);
+}
+
+function onMessage(e) {
+    const msgValue = e.currentTarget.value;
+    localStorage.setItem('feedback', msgValue);
+}
+
+function savedTextarea(e) {
+    const textareaMessage = localStorage.getItem('feedback')
+    if (textareaMessage) {
+        message.value = textareaMessage;
+    }  
+}
+function savedEmail(e) {
+    const savedMail = localStorage.getItem('email')
+    if (savedMail) {
+        input.value = savedMail;
+    }  
+}
